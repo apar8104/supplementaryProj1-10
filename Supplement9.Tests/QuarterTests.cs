a8b82ada0b5952bcd45e4d3c320d1b6312250ce0
apar8104 <apar8104@students.vsu.edu>
2025-02-27 11:42:38 -0500

    test: create QuarterEqualityOperatorWorksCorrectly test

--- Supplement9.Tests/QuarterTests.cs
+++ Supplement9.Tests/QuarterTests.cs
@@ -1,1 +1,19 @@
 namespace Supplement9;
+
+public class QuarterTests
+{
+    [Theory]
+    [InlineData(0.1f, 0.2f, true)]  
+    [InlineData(0.3f, 0.4f, true)]  
+    [InlineData(0.6f, 0.7f, true)]  
+    [InlineData(0.8f, 0.9f, true)]  
+    [InlineData(0.1f, 0.3f, false)] 
+    [InlineData(0.4f, 0.6f, false)] 
+    public void QuarterEqualityOperatorWorksCorrectly(float value1, float value2, bool expected)
+    {
+        var q1 = new Quarter(value1);
+        var q2 = new Quarter(value2);
+        Assert.Equal(expected, q1 == q2);
+    }
+    
+}
