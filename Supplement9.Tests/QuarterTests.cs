7a7133f9f684c6977c4bcd6083d2350266488672
apar8104 <apar8104@students.vsu.edu>
2025-02-27 11:36:56 -0500

    chore: created Quarter class and QuarterTests

---
+++ Supplement9.Tests/QuarterTests.cs
@@ -0,0 +1,1 @@
+namespace Supplement9;
---
+++ Supplement9/Quarter.cs
@@ -0,0 +1,55 @@
+namespace Supplement9;
+public class Quarter
+{
+    private readonly float _value;
+
+    public Quarter(float value)
+    {
+        throw new NotImplementedException();
+    }
+
+    private int GetQuarter()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static bool operator ==(Quarter q1, Quarter q2)
+    {
+        throw new NotImplementedException();
+    }
+
+     public static bool operator !=(Quarter q1, Quarter q2)
+    {
+        throw new NotImplementedException();
+    }
+
+     public static bool operator <(Quarter q1, Quarter q2)
+    {
+        throw new NotImplementedException();
+    }
+
+     public static bool operator >(Quarter q1, Quarter q2)
+    {
+        throw new NotImplementedException();
+    }
+
+     public static bool operator <=(Quarter q1, Quarter q2)
+    {
+        throw new NotImplementedException();
+    }
+
+     public static bool operator >=(Quarter q1, Quarter q2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Equals(object obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+}
