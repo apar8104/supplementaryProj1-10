a5861a0dd0baa67fce0842c7bba39b55773fa176
apar8104 <apar8104@students.vsu.edu>
2025-02-27 10:55:06 -0500

    test: created EnumeratorShouldYieldFloatsBetween0And1 test

--- Supplement9.Tests/RandomFloatEnumerableTests.cs
+++ Supplement9.Tests/RandomFloatEnumerableTests.cs
@@ -1,1 +1,19 @@
+using System.Collections.Generic;
 namespace Supplement9.Tests;
+
+public class RandomFloatEnumerableTests
+{
+    [Fact]
+    public void EnumeratorShouldYieldFloatsBetween0And1()
+    {
+        var generator = new RandomFloatEnumerable();
+        using var enumerator = generator.GetEnumerator();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Assert.True(enumerator.MoveNext());
+            float value = enumerator.Current;
+            Assert.InRange(value, 0.0f, 1.0f);
+        }
+    }
+}
