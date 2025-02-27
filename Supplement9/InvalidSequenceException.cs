3b05bf26aa84ffac4e08dde0a5fc223d8f78a5d5
apar8104 <apar8104@students.vsu.edu>
2025-02-27 10:31:10 -0500

    doc: added documentation to basic invalid sequence function

--- Supplement9/InvalidSequenceException.cs
+++ Supplement9/InvalidSequenceException.cs
@@ -2,9 +2,17 @@
 
 public class InvalidSequenceException : Exception
 {
+    /// <summary>
+    /// This gives the invalid sequence message when the invalid sequence
+    /// excepton is thrown. 
+    /// </summary>
     public InvalidSequenceException()
         : base("An invalid sequence was generated.") {}
     
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
     public InvalidSequenceException(string message)
         : base(message) {}
 }
