252ffd208f36f35a784615801d6d12be823b4645
apar8104 <apar8104@students.vsu.edu>
2025-02-27 12:08:50 -0500

    funct: added functionality to greater than  operators in Quarter class

--- Supplement9/Quarter.cs
+++ Supplement9/Quarter.cs
@@ -33,13 +33,11 @@ public class Quarter
 
      public static bool operator >(Quarter q1, Quarter q2)
     {
-        throw new NotImplementedException();
+        if (q1 is null || q2 is null) throw new ArgumentNullException();
+        return q1._value > q2._value;
     }
 
-     public static bool operator >=(Quarter q1, Quarter q2)
-    {
-        throw new NotImplementedException();
-    }
+     public static bool operator >=(Quarter q1, Quarter q2) => q1 > q2 || q1 == q2;
 
     public override bool Equals(object obj)
     {
