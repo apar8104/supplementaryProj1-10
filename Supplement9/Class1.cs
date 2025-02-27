f2703d6372a704074b35bce5f1bef32aeccd5892
apar8104 <apar8104@students.vsu.edu>
2025-02-27 09:09:31 -0500

    Chore: created InvalidSequenceException class

--- Supplement9/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Supplement9;
-
-public class Class1
-{
-
-}
---
+++ Supplement9/InvalidSequenceException.cs
@@ -0,0 +1,13 @@
+﻿namespace Supplement9;
+
+public class InvalidSequenceException : Exception
+{
+    public InvalidSequenceException()
+    {
+        throw new NotImplementedException();
+    }
+    public InvalidSequenceException(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
