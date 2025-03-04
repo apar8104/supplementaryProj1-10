04c72108590cdd4b270708291580c675357a85e2
apar8104 <apar8104@students.vsu.edu>
2025-03-03 23:40:20 -0500

    test: created AddingValidQuarterDisplaysCorrectly

---
+++ Supplement9.Tests/QuarterAppTests.cs
@@ -0,0 +1,16 @@
+namespace Supplement9.Tests;
+
+public class QuarterAppTests
+{
+    [Fact]
+    public void AddingValidQuarterDisplaysCorrectly()
+    {
+        var app = new QuarterApp();
+        app.AddQuarter(0.1f);
+        app.AddQuarter(0.3f);
+
+        var output = app.GetQuartersDisplay();
+        Assert.Contains("0.0 - 0.25", output);
+        Assert.Contains("0.25 - 0.5", output);
+    }
+}
