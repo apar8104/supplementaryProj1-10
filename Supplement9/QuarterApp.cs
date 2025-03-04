c8c69d5d653e8491ab790976bb79a35984e6a813
apar8104 <apar8104@students.vsu.edu>
2025-03-03 23:34:24 -0500

    chore: created QuarterApp class

---
+++ Supplement9/QuarterApp.cs
@@ -0,0 +1,24 @@
+namespace Supplement9;
+public class QuarterApp
+{
+    public void Run()
+    {
+        throw new NotImplementedException();
+    }
+    public void AddQuarter(float value)
+    {
+        throw new NotImplementedException();
+    }
+    public string GetQuartersDisplay()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        new QuarterApp().Run();
+    }
+}
